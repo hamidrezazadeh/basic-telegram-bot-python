002269c313eeb720f9a68ad08faa18ff048e55ac
hamidrezazadeh <seyyedhamid.rezazadeh@gmail.com>
2023-11-29 20:08:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,21 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, InlineQueryHandler, Filters
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from configparser import ConfigParser
+from telegram.ext import Updater, CommandHandler
+import os
 
-# Ref: http://fygul.blogspot.com/2017/07/configparser.html
-cfg = ConfigParser()
-cfg.read('config.ini')
-token = cfg['token']['key']
-
-updater = Updater(token=token, use_context=True)
-dispatcher = updater.dispatcher
-
-# To catch commands like "/start"
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Let's build a telegram bot that help people learn English faster")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def main():
+    # Use environment variable for token for better security; replace with your token for testing
+    token = os.environ.get('BOT_TOKEN', '6774835592:AAFI-egcgqCttSq5V9W8QHxQAgbf4LY_cMU')
 
-# To repeat what the user said
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
 
-echo_handler = MessageHandler(Filters.text and (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-# To capitalize what the user inputted after /caps
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    updater.start_polling()
+    updater.idle()
 
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
-
-# To captialize words using inline mode
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
-
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
-
-# Unknown commands
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
-
-# To start the bot
-updater.start_polling()
-print("The bot is working.")
-
-# To end the bot
-updater.idle()
-print("The bot is stopped.")
+if __name__ == '__main__':
+    main()

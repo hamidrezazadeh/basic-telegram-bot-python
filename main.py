acab0628de8c509494f6d02d16b647807d94766c
hamidrezazadeh <seyyedhamid.rezazadeh@gmail.com>
2023-11-29 20:43:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import openai
-import os
 
 def handle_message(update, context):
     user_message = update.message.text
 
-    # OpenAI API call
+    # Fetching the API key from environment variables
+    openai_api_key = os.environ.get('OPENAI_API_KEY')
+
+    openai.api_key = openai_api_key
+
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # or another model you have access to
+        model="gpt-3.5-turbo",  # Adjust the model as per your access
         messages=[{"role": "user", "content": user_message}]
     )
 
@@ -15,14 +19,11 @@ def handle_message(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=response.choices[0].message['content'])
 
 def main():
-    # Use environment variable for token
-    token = os.environ.get('BOT_TOKEN', '6774835592:AAFI-egcgqCttSq5V9W8QHxQAgbf4LY_cMU')
-    openai.api_key = os.environ.get('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')  # Set your OpenAI API key
-
+    token = os.environ.get('BOT_TOKEN', 'YOUR_TELEGRAM_BOT_TOKEN')
+    
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Message handler
     message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
     dispatcher.add_handler(message_handler)
 
